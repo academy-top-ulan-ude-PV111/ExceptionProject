66e017ea09a5df43698f5eaeb0667d4b2f9cb6e2
GitStudent2022 <torelgame@mail.ru>
2022-09-30 14:15:15 +0300

    Добавьте файлы проекта.

---
+++ ExceptionProject/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ExceptionProject
+{
+    internal class Program
+    {
+        static void Run()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            try
+            {
+                int a;
+                a = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(10 / a);
+                Console.WriteLine(array[a]);
+
+            }
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.Message + ex.Source);
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            catch (Exception ex)
+            {
+                Console.WriteLine("Run catch");
+                Console.WriteLine("Message " + ex.Message);
+                Console.WriteLine("Source " + ex.Source);
+                Console.WriteLine("TargetSite " + ex.TargetSite);
+                Console.WriteLine("StackTrace " + ex.StackTrace);
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finally block");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Run();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine("\nMain catch");
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.Source);
+            //    Console.WriteLine(ex.TargetSite);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+
+            //Console.WriteLine("Program work");
+
+            try
+            {
+                Fraction fraction = new Fraction(10, 20);
+
+                fraction.Denominator = 0;
+            }
+            catch(FractionException ex)
+            {
+                if (ex.Code == 100)
+                    Console.WriteLine(ex.Message + " error in conctructor!");
+                else if (ex.Code == 200)
+                    Console.WriteLine(ex.Message + " error in property set!");
+                else
+                    Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ ExceptionProject/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionProject
+{
+    public class FractionException : Exception
+    {
+        public int Code { get; }
+        public FractionException(string message, int code) : base(message) 
+        {
+            Code = code;
+        }
+    }
+    public class Fraction
+    {
+        public int Numerator { set; get; }
+        
+        int denominator;
+        public int Denominator
+        {
+            set
+            {
+                if (value != 0)
+                    denominator = value;
+                else
+                    throw new FractionException("Denominator not null!", 200);
+            }
+            get => denominator;
+        }
+            
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            if (denominator != 0)
+                Denominator = denominator;
+            else
+                throw new FractionException("Denominator not null!", 100);
+        }
+    }
+
+    public class User
+    {
+        int age;
+        public int Age { 
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new Exception("Age bad value");
+                else
+                    age = value;
+            }
+            get => age;
+        }
+    }
+}
